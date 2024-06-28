803b36b9eeaafaa620e7a8012e8a2467883a9e37
sango366 <txobitxobi.1@gmail.com>
2024-06-29 03:07:13 +0900

    first commit

---
+++ db/mysql_setup.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS container_final CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE USER IF NOT EXISTS 'repuser'@'%' IDENTIFIED BY 'repuser';
+GRANT ALL PRIVILEGES ON *.* TO 'repuser'@'%';
