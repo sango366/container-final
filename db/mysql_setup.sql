6f7f83b28ccbe5212da5a51eca653cdd0a5cf07a
sango366 <txobitxobi.1@gmail.com>
2024-06-29 23:49:48 +0900

    dockerfileのID周りの修正

--- db/mysql_setup.sql
+++ db/mysql_setup.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS container_final CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER IF NOT EXISTS 'repuser'@'localhost' IDENTIFIED BY 'reppass';
-GRANT ALL PRIVILEGES ON *.* TO 'repuser'@'localhost';
+CREATE USER IF NOT EXISTS 'repuser'@'%' IDENTIFIED BY 'reppass';
+GRANT ALL PRIVILEGES ON *.* TO 'repuser'@'%';
